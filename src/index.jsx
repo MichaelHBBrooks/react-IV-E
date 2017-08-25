68a6339f01004c4ea59481ddd3c002af66b9a5ef
MichaelHBBrooks <Michael.H.B.Brooks@gmail.com>
2017-08-25 10:13:12 -0400

    Removed vestigial code.

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,4 @@
-// // Application entrypoint.
-
-// // Load up the application styles
-// require("../styles/application.scss");
-
-// // Render the top-level React component
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App.jsx';
-
-// ReactDOM.render(<App />, document.getElementById('react-root'));
-
-
-import axios from 'axios';
+// import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Header } from './Header.jsx';
